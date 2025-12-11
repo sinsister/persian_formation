73804adbc0516b2b157e739ed6b8d83732a3516a
Your Name <you@example.com>
2025-12-11 17:06:11 +0330

    fix

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ async def show_hall_of_fame_to_user(message_or_callback):
             "به زودی قهرمانان لیگ‌ها مشخص می‌شوند."
         )
     else:
-        header = "🏆 *قهرمان های تورنومنت ولی های های*\n𝐏𝐄𝐑𝐒𝐈𝐀𝐍 𝐅𝐎𝐑𝐌𝐀𝐓𝐈𝐎𝐍🏆\n\n"
+        header = "قهرمان های تورنومنت ها ولیگ های 𝐏𝐄𝐑𝐒𝐈𝐀𝐍 𝐅𝐎𝐑𝐌𝐀𝐓𝐈𝐎𝐍 🏆"
         
         champions_text = ""
         for league_name, champ_username, champ_display, set_date in champions:
